f6997623edac02b1d37c3f21f477b5a1a08e4a3f
Gabriel Luiz F Souto <gabrielluizferraz@gmail.com>
2025-11-03 22:23:02 +0100

    Implement BFS in ClosestDotSearchAgent and return food count in AnyFoodSearchProblem

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -587,7 +587,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.bfs(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -623,7 +623,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1: Tuple[int, int], point2: Tuple[int, int], gameState: pacman.GameState) -> int:
     """
